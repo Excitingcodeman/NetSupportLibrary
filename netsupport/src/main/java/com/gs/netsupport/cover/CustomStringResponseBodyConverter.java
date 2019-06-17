cd707394105d8beb14e6342cb0c911cba017e264
huangjj <hjj05190@gmail.com>
2019-06-17 14:24:56 +0800

    打印返回体

--- netsupport/src/main/java/com/gs/netsupport/cover/CustomStringResponseBodyConverter.java
+++ netsupport/src/main/java/com/gs/netsupport/cover/CustomStringResponseBodyConverter.java
@@ -1,7 +1,9 @@
 package com.gs.netsupport.cover;
 
+import android.util.Log;
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
+import com.gs.netsupport.BuildConfig;
 import okhttp3.ResponseBody;
 import retrofit2.Converter;
 
@@ -17,18 +19,21 @@ import java.io.IOException;
  */
 public class CustomStringResponseBodyConverter<T> implements Converter<ResponseBody, T> {
 
-
+    static String TAG = CustomStringResponseBodyConverter.class.getSimpleName();
     private final TypeAdapter<T> adapter;
 
     CustomStringResponseBodyConverter(Gson gson, TypeAdapter<T> adapter) {
         this.adapter = adapter;
+
     }
 
 
     @Override
     public T convert(ResponseBody value) throws IOException {
         String response = value.string();
-
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, response);
+        }
         try {
             return adapter.fromJson(response);
         } finally {
